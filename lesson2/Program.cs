4b5deb7a5ae1cac9888bfacbe7ddb7dc2fd0d4cb
ponuch <ponuch.lug@gmail.com>
2022-09-01 15:55:56 +0300

    Add second and third tasks

--- lesson2/Program.cs
+++ lesson2/Program.cs
@@ -1,6 +1,12 @@
-﻿
+﻿using System.Collections;
+
+// *****************************************************************************************************************************
+// Задача 10: Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа. *
+// *****************************************************************************************************************************
+
+
 Console.WriteLine("Input number");
-int number = Math.Abs(Int32.Parse(Console.ReadLine())); //  skip any errors, assume that number is real
+int number = Math.Abs(Int32.Parse(Console.ReadLine())); //  skip any errors, let assume that input number is real
 
 // check number and calculate second digit
 if (validate(number)) {
@@ -21,4 +27,62 @@ bool validate( int num) {
 int findDigit(int num) {
     int intermediateResult = num / 10;
     return intermediateResult % 10;
+}
+
+// ******************************************************************************************************************************
+// Задача 13: Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.             *
+// ******************************************************************************************************************************
+
+
+
+Console.WriteLine("Input number");
+int checkedNumber = Math.Abs(Int32.Parse(Console.ReadLine())); //  skip any errors, let assume that input number is real
+if(validateNumber(checkedNumber)) {
+    int[] arr = parseNumber(checkedNumber);
+    Console.WriteLine("Third digit = {0:G}", arr[2]);
+}
+else {
+    Console.WriteLine ("Wrong number, it must be greater than 99");
+}
+
+int[] parseNumber(int num) {
+    var list = new List<int>();
+    while (num > 0) {
+        var digit = num % 10;
+        num /= 10;
+        list.Add(digit);
+    }
+    list.Reverse();
+    return list.ToArray<int>();
+}
+
+
+bool validateNumber( int num) {
+    if (num > 99) {
+        return true;
+    }
+    return false;
+}
+
+// ***************************************************************************************************************************************
+// Задача 15: Напишите программу, которая принимает на вход цифру, обозначающую день недели, и проверяет, является ли этот день выходным.*
+//****************************************************************************************************************************************
+Console.WriteLine("Input the day of the week");
+int dayOfWeek = Math.Abs(Int32.Parse(Console.ReadLine())); //  skip any errors, let assume that input number is real
+
+if (checkDay(dayOfWeek)) {
+    Console.WriteLine("It's a weekend, you have to go to the bar!");
+}
+else {
+    Console.WriteLine("Unfortunally, you have to work :(");
+}
+
+bool checkDay(int dayOfWeek) {
+    switch (dayOfWeek) {
+        case 6: 
+        case 7:
+            return true;
+        default:
+            return false;
+    }
 }
