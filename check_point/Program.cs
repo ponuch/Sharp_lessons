8102b2d35e661fdbc857386b5c816586a3cae950
ponuch <ponuch.lug@gmail.com>
2022-10-03 18:10:34 +0300

    release 1.0

--- check_point/Program.cs
+++ check_point/Program.cs
@@ -1,2 +1,39 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿string[] words = {"Hello", "2", "world", ":-)", "123232", "qwe", "qwerty", "абырвалг"};
+string[] resultWords = new string[GetResultArraySize(words)];
+PrintStringArray(words);
+
+FillResultArray(words, resultWords);
+
+PrintStringArray(resultWords);
+
+int GetResultArraySize(string[] arr) {
+    int size = 0;
+    for (int i = 0; i < arr.Length; i++) {
+        if (words[i].Length <= 3) {
+            size++;
+        }
+    }
+    return size;
+}
+
+void FillResultArray(string[] arr, string[] result) {
+    int index = 0;
+    for (int i = 0; i < arr.Length; i++) {
+        if (arr[i].Length <= 3) {
+            result[index] = arr[i];
+            index++;
+        }
+    }
+}
+
+void PrintStringArray(string[] arr) {
+    for (int i = 0; i < arr.Length; i++) {
+        Console.Write($"{arr[i]} ");
+    }
+    Console.WriteLine();
+}
+
+
+
+
+
